a11ae636ebec04a021a2514313a2f8cd558e208a
meavv <c.c.c.@bk.ru>
2021-11-22 14:11:13 +0500

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,43 @@
+package ru.job4j.ref;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> list = new ArrayList<>();
+        users.values().forEach(a -> list.add(User.of(a.getName())));
+        return list;
+    }
+
+}
+
+class ShareNotSafe {
+    public static void main(String[] args) throws InterruptedException {
+        UserCache cache = new UserCache();
+        User user = User.of("name");
+        cache.add(user);
+        Thread first = new Thread(
+                () -> {
+                    user.setName("rename");
+                }
+        );
+        first.start();
+        first.join();
+        System.out.println(cache.findById(1).getName());
+    }
+}
