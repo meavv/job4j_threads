985ed0a5a1bee665d54b39a6b60fe5950bbe9217
meavv <c.c.c.p@bk.ru>
2021-11-23 16:11:39 +0500

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public class ParseFile {
+
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+
+    public String getContent(Predicate<Character> filter) {
+        StringBuilder output = new StringBuilder();
+        try (InputStream i = new FileInputStream(file)) {
+            int data;
+            while ((data = i.read()) > 0) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return output.toString();
+    }
+
+
+    public void saveContent(String content) {
+        try (OutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
