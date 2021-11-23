81bf502a9bdbae6b387a685700a4b5ed802e2931
meavv <c.c.c.p@bk.ru>
2021-11-23 21:33:33 +0500

    3. Класс хранилища пользователей UserStorage [#1104 #231961]

--- src/main/java/ru/job4j/ref/UserStorage.java
+++ src/main/java/ru/job4j/ref/UserStorage.java
@@ -9,41 +9,26 @@ import java.util.HashMap;
 public class UserStorage {
 
 
-
     @GuardedBy("this")
     private final HashMap<Integer, User> userList = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        if (!userList.containsKey(user.getId())) {
-            userList.put(user.getId(), user);
-            return userList.containsValue(user);
-        }
-        return false;
+        return userList.putIfAbsent(user.getId(), user) == null;
     }
 
     public synchronized boolean update(User user) {
-        if (userList.containsKey(user.getId())) {
-            userList.put(user.getId(), user);
-            return true;
-        }
-        return false;
+        return userList.replace(user.getId(), user) != null;
     }
 
     public synchronized boolean delete(User user) {
-        if (userList.containsValue(user)) {
-            userList.remove(user.getId());
-            return true;
-        }
-        return false;
+        return userList.remove(user.getId(), user);
     }
 
-    public  synchronized boolean transfer(int fromId, int toId, int amount) {
+    public synchronized boolean transfer(int fromId, int toId, int amount) {
         User userTo = userList.get(toId);
         User userFrom = userList.get(fromId);
-        UserStorage userStorage = new UserStorage();
         if (userTo != null && userFrom != null && userFrom.getAmount() >= amount) {
             userFrom.setAmount(userFrom.getAmount() - amount);
-            userStorage.update(userFrom);
             userTo.setAmount(userTo.getAmount() + amount);
             return true;
         }
