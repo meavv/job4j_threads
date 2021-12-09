14830993db306f84996ba15b62401301767ade7e
meavv <c.c.c.p@bk.ru>
2021-12-09 15:36:33 +0500

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -50,37 +50,34 @@ public class RolColSum {
             }
             array[i] = new Sums(rowSum, colSum);
         }
-        System.out.println("Standart " + Arrays.toString(array));
+        System.out.println("Sum " + Arrays.toString(array));
         return array;
     }
 
     public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
         Sums[] array = new Sums[(matrix.length)];
-        for (int i = 0; i < matrix.length; i++) {
-            System.out.println(run(matrix));
+        for (int a = 0; a < array.length; a++) {
+            array[a] = run(matrix, a).get();
         }
+        System.out.println(Arrays.toString(array));
         return array;
     }
 
-    public static CompletableFuture<Sums> run(int[][] array) {
+    public static CompletableFuture<Sums> run(int[][] array, int i) {
         return CompletableFuture.supplyAsync(() -> {
-            var rsl = new Sums(0, 0);
-            for (int i = 0; i < array.length; i++) {
-                int rowSum = 0;
-                int colSum = 0;
-                for (int j = 0; j < array[i].length; j++) {
-                    rowSum += array[i][j];
-                    colSum += array[j][i];
-                }
-                rsl = new Sums(rowSum, colSum);
-                return rsl;
+            int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < array[i].length; j++) {
+                rowSum += array[i][j];
+                colSum += array[j][i];
             }
-            return rsl;
+            return new Sums(rowSum, colSum);
         });
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         int[][] twoDimArray1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        RolColSum.sum(twoDimArray1);
         RolColSum.asyncSum(twoDimArray1);
     }
 
