773ba27f73e473089ecbd56ece31b029005d3ca2
meavv <c.c.c.p@bk.ru>
2021-12-09 13:04:11 +0500

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pool/MergeSort.java
@@ -0,0 +1,60 @@
+package ru.job4j.pool;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    /**
+    при следующем условии, массив из одного элемента
+    делить нечего, возвращаем элемент
+    попали сюда, значит в массиве более одного элемента
+    находим середину
+    объединяем отсортированные части
+    сортируем левую часть
+    сортируем правую часть
+    Метод объединения двух отсортированных массивов
+    **/
+
+    public static int[] sort(int[] array) {
+        return sort(array, 0, array.length - 1);
+    }
+
+    private static int[] sort(int[] array, int from, int to) {
+        if (from == to) {
+            return new int[]{
+                    array[from]
+            };
+        }
+        int mid = (from + to) / 2;
+        return merge(
+                sort(array, from, mid),
+                sort(array, mid + 1, to)
+        );
+    }
+
+    public static int[] merge(int[] left, int[] right) {
+        int li = 0;
+        int ri = 0;
+        int resI = 0;
+        int[] result = new int[left.length + right.length];
+        while (resI != result.length) {
+            if (li == left.length) {
+                result[resI++] = right[ri++];
+            } else if (ri == right.length) {
+                result[resI++] = left[li++];
+            } else if (left[li] <= right[ri]) {
+                result[resI++] = left[li++];
+            } else {
+                result[resI++] = right[ri++];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] aray = {1, 10, 9, 8, 2, 4, 5, 3, 6};
+        System.out.println(Arrays.toString(aray));
+        System.out.println(Arrays.toString(MergeSort.sort(aray)));
+    }
+
+}
---
+++ src/main/java/ru/job4j/pool/SearchElement.java
@@ -0,0 +1,51 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class SearchElement extends RecursiveTask<Integer> {
+
+    private final int[] array;
+    private final int index;
+    private final int from;
+    private final int to;
+
+    public SearchElement(int[] array, int index, int from, int to) {
+        this.array = array;
+        this.index = index;
+        this.from = from;
+        this.to = to;
+    }
+
+
+    @Override
+    protected Integer compute() {
+        if (to - from < 10) {
+            for (int i = from; i < to; i++) {
+                if (array[i] == index) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+        int mid = (from + to) / 2;
+        SearchElement l = new SearchElement(array, index, from, mid);
+        SearchElement r = new SearchElement(array, index, mid + 1, to);
+        l.fork();
+        r.fork();
+        return Math.max(l.join(), r.join());
+    }
+
+    public static Integer search(int[] array, int index) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new SearchElement(array, index, 0, array.length - 1));
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {1, 10, 2, 3, 7, 5, 9, 17, 18, 19, 101, 15489, 45, 99, 88, 77};
+        var x = SearchElement.search(array, 101);
+        System.out.println(x);
+    }
+
+}
